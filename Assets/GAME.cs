e50747e1401aef17578ced26e4077587d6004966
SunQiana <71679899+SunQiana@users.noreply.github.com>
2020-11-16 04:35:02 +0800

    Initial commit

---
+++ Assets/GAME.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GAME : MonoBehaviour
+{
+    public void Click()
+    {
+        Debug.Log("test");
+    }
+    
+
+}
