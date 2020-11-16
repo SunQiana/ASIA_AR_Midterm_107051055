aa8e4439510355237a7735e14cda1d89e1c1f297
SunQiana <71679899+SunQiana@users.noreply.github.com>
2020-11-16 17:22:08 +0800

    11/16/17:22

---
+++ Assets/RPG Monster Duo PBR Polyart/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   public Transform Turtle;
+   public Transform Slime;
+
+     Animator TurtleAnim;
+     Animator SlimeAnim;
+
+     void Start()
+     {
+         TurtleAnim=Turtle.GetComponent<Animator>();
+         SlimeAnim=Slime.GetComponent<Animator>();
+     }
+
+     public void AttackAnim()
+     {
+       TurtleAnim.SetTrigger("攻擊");
+       SlimeAnim.SetTrigger("攻擊");
+     }
+     public void GetHurt()
+     {
+       TurtleAnim.SetTrigger("受攻擊");
+       SlimeAnim.SetTrigger("受攻擊");
+     }
+     public void Happy()
+     {
+       TurtleAnim.SetTrigger("高興");
+       SlimeAnim.SetTrigger("高興");
+     }
+     public void Stun()
+     {
+       TurtleAnim.SetBool("暈眩");
+       SlimeAnim.SetBool("暈眩");
+     }
+
+}
